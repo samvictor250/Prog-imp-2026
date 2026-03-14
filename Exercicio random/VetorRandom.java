fd744e887f71263524ad97058c95f2c171ebb829
Samuel Victor Hill Santos <samuel.00000860090@unicap.br>
2026-03-14 00:20:35 +0000

    prototipo insertion e bubble

--- Exercicio random/VetorRandom.java
+++ Exercicio random/VetorRandom.java
@@ -2,18 +2,52 @@ import java.util.Random;
 
 public class VetorRandom{
   public static Random rand = new Random();
-  public static int[] vetor = new int[100];
+  public static int[] vetor = new int[10];
 
   public static void main(String[] args){
-
+    gerarAleatorios();
+    //TODO: Usar Scanner pra buscar numero do vetor
+    buscaSequencial();
+  }
+  
+  public static void gerarAleatorios(){
+    
     System.out.print("{");
+    
     if(vetor.length >= 1){
+
       System.out.print(" " + vetor[0]);
       for(int i = 0; i < vetor.length; i++){
         int MAX = rand.nextInt(31);
         System.out.print(", " + MAX);
       }
+      
     }
-    System.out.print("}\n");
+
+    System.out.print(" }\n");
   }
+
+  public static int buscaSequencial(int x, int[] vetor){
+
+    for(int i = 0; i < vetor.length; i++){
+      if(x == vetor[i]){
+        return i;
+      }
+    }
+    
+    return -1;
+  }
+
+  // Bubble sort 
+  public static int preencherAleatorioOrdenado(int[] v, int n, int x){
+
+  }
+
+  // Insertion sort
+  public static int inserirOrdenado(int[] vetor, int n, int x){
+    int pos = n;
+    while(x < v)
+  }
+
+  // Binary Search
 }
