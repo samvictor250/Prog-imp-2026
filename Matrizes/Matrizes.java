56778f6814f084bed7decdb09b34534d90c91ddd
Samuel Victor Hill Santos <samuel.00000860090@unicap.br>
2026-04-29 02:01:30 +0000

    Menor matriz implementado

--- Matrizes/Matrizes.java
+++ Matrizes/Matrizes.java
@@ -5,21 +5,38 @@ class Matrizes{
   public static int[][] m = new int[3][4];
 
   public static void main(String[] args){
-    
+    inserirImprimirDados();
+    menorMatriz(m);
+  }
+  
+  public static void inserirImprimirDados(){
     for(int i = 0; i < m.length; i += 1){
       for(int j = 0; j < m[0].length; j++){
         System.out.printf("Digite a %dº linha / %dº valor : ", i+1, j+1);
         m[i][j] = input.nextInt();
-        m[2][1] = 55;
+        // m[2][1] = 55;
       }
     }
 
     for(int i = 0; i < m.length; i += 1){
       for(int j = 0; j < m[0].length; j += 1){
-          System.out.print(m[i][j] + " ");        
+        System.out.print(m[i][j] + " ");        
       }
       System.out.print("\n");
+      }
+    }  
+    
+    public static int menorMatriz(int[][] m){
+      
+      int menor = Integer.MAX_VALUE;
+      
+      for(int i = 0; i < m.length; i++){
+        for(int j = 0; j < m[0].length; i++){
+          if(m[i][j] < menor){
+            menor = m[i][j];
+          }
+        }
+      }
+      return menor;
     }
-
-  }
 }
